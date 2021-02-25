d82fb6f550dee6a9d1adcf2b0a35a40d1f585f42
asiergr <asier.garciaruiz@gmail.com>
2021-02-25 12:19:54 -0500

    Added function

---
+++ src/euler-murayama.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+
+template<typename Function>
+static int* euler_murayama(int interval[2], int points, double x0, Function fn) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0,1.0);
+    // dt terms
+    double dt = (interval[0] + interval[1]) / 2;
+    double dW = distribution(generator) * sqrt(dt);
+
+    // params for Ornsrein-Uhlenbeck function
+    double theta = 0.7;
+    double mu = 1.5;
+    double sigma = 0.06;
+
+    // output
+    int out[points + 1];
+    out[0] = x0;
+
+    for (int i = 1; i < points; i++) {
+        double t = interval[0] + (i - 1)*dt;
+        double y_i = out[i - 1];
+
+        out[i] = y_i + (theta*(mu - y_i))*dt + sigma * dW;
+
+    }
+}
